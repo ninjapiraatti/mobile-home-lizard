6699cc8280a33cf7167b70d46a20e25ba8cdea12
Tuomas Louekari <tuoppil@gmail.com>
2025-10-11 13:29:52 +0300

    Update to esp-hal rc0

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,36 +1,36 @@
 #![no_std]
 #![no_main]
 
-use embedded_hal::pwm::SetDutyCycle;
-use embedded_hal::digital::OutputPin;
+use core::cell::RefCell;
+use critical_section::Mutex;
 use embedded_graphics::{
     mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
     pixelcolor::BinaryColor,
     prelude::*,
     text::{Baseline, Text},
 };
-use core::cell::RefCell;
-use critical_section::Mutex;
-use esp_hal::{clock::CpuClock, Blocking};
+use embedded_hal::digital::OutputPin;
+use embedded_hal::pwm::SetDutyCycle;
 use esp_hal::main;
 use esp_hal::timer::timg::TimerGroup;
-use esp_println::println;
+use esp_hal::{clock::CpuClock, Blocking};
 use esp_hal::{
     delay::Delay,
-    gpio::{Level, Input, InputConfig, Event, Output, Io, OutputConfig, Pull},
-    time::Rate,
-    rmt::Rmt,
+    gpio::{Event, Input, InputConfig, Io, Level, Output, OutputConfig, Pull},
     handler,
+    i2c::master::{Config, I2c},
     ram,
-    i2c::master::{I2c, Config},
+    rmt::Rmt,
+    time::Rate,
 };
-use esp_hal_smartled::{smartLedBuffer, SmartLedsAdapter};
+use esp_hal_smartled::{smart_led_buffer, SmartLedsAdapter};
+use esp_println::println;
+use rotary_encoder_hal::{Direction, Rotary};
 use smart_leds::{
     brightness, gamma,
     hsv::{hsv2rgb, Hsv},
     SmartLedsWrite,
 };
-use rotary_encoder_hal::{Direction, Rotary};
 use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
 
 struct MenuItem<'a> {
@@ -64,7 +64,7 @@ impl<'a> Menu<'a> {
                         self.scroll_offset += 1;
                     }
                 }
-            },
+            }
             Direction::CounterClockwise => {
                 if self.selected_index > 0 {
                     self.selected_index -= 1;
@@ -72,50 +72,49 @@ impl<'a> Menu<'a> {
                         self.scroll_offset -= 1;
                     }
                 }
-            },
-            Direction::None => {},
+            }
+            Direction::None => {}
         }
     }
-    
+
     fn get_selected_item(&self) -> Option<&MenuItem<'a>> {
         self.items.get(self.selected_index)
     }
-    
+
     fn render(&self, i2c: &mut I2c<Blocking>) {
         let text_style = MonoTextStyleBuilder::new()
-        .font(&FONT_6X10)
-        .text_color(BinaryColor::On)
-        .build();
+            .font(&FONT_6X10)
+            .text_color(BinaryColor::On)
+            .build();
 
         let interface = I2CDisplayInterface::new(i2c);
-        let mut display = Ssd1306::new(
-            interface,
-            DisplaySize128x32,
-            DisplayRotation::Rotate0,
-        ).into_buffered_graphics_mode();
+        let mut display = Ssd1306::new(interface, DisplaySize128x32, DisplayRotation::Rotate0)
+            .into_buffered_graphics_mode();
         if let Err(e) = display.clear(BinaryColor::Off) {
             println!("Failed to clear display");
         }
-        
-        let visible_items = self.items.iter()
+
+        let visible_items = self
+            .items
+            .iter()
             .skip(self.scroll_offset)
             .take(self.max_visible_items);
-            
+
         for (i, item) in visible_items.enumerate() {
             let y_position = (i as i32) * 10 + 2; // 10 pixels per row, 2 pixel padding
             let is_selected = self.scroll_offset + i == self.selected_index;
 
             if is_selected {
                 Text::with_baseline(">", Point::new(0, y_position), text_style, Baseline::Top)
-                .draw(&mut display)
-                .unwrap();
+                    .draw(&mut display)
+                    .unwrap();
             }
 
             Text::with_baseline(
-                item.text, 
-                Point::new(8, y_position), 
-                text_style, 
-                Baseline::Top
+                item.text,
+                Point::new(8, y_position),
+                text_style,
+                Baseline::Top,
             )
             .draw(&mut display)
             .unwrap();
@@ -123,10 +122,10 @@ impl<'a> Menu<'a> {
             if let Some(value) = item.value {
                 let value_text = alloc::format!("{}", value);
                 Text::with_baseline(
-                    &value_text, 
-                    Point::new(90, y_position), 
-                    text_style, 
-                    Baseline::Top
+                    &value_text,
+                    Point::new(90, y_position),
+                    text_style,
+                    Baseline::Top,
                 )
                 .draw(&mut display)
                 .unwrap();
@@ -157,6 +156,7 @@ fn panic(_: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
+esp_bootloader_esp_idf::esp_app_desc!();
 extern crate alloc;
 
 #[main]
@@ -168,7 +168,7 @@ fn main() -> ! {
     let peripherals = esp_hal::init(config);
     //let peripherals = esp_hal::init(esp_hal::Config::default());
     let mut io = Io::new(peripherals.IO_MUX);
-    
+
     // Interrupt stuff
     let config = InputConfig::default().with_pull(Pull::Up);
     let switch = peripherals.GPIO20;
@@ -188,12 +188,12 @@ fn main() -> ! {
 
     //let mut test_pin = Output::new(peripherals.GPIO7, Level::Low, OutputConfig::default());
     //let mut led_pin_a = Output::new(peripherals.GPIO8, Level::Low, OutputConfig::default());
-    
+
     // LED stuff
     let led_pin = peripherals.GPIO8;
     let led_freq = Rate::from_mhz(80);
     let rmt = Rmt::new(peripherals.RMT, led_freq).unwrap();
-    let rmt_buffer = smartLedBuffer!(1);
+    let rmt_buffer = smart_led_buffer!(1);
     let mut led = SmartLedsAdapter::new(rmt.channel0, led_pin, rmt_buffer);
     let mut color = Hsv {
         hue: 0,
@@ -202,7 +202,6 @@ fn main() -> ! {
     };
     let mut data;
     println!("Did RGB LED stuff");
-
 
     // OLED stuff
     let mut i2c = I2c::new(
@@ -228,32 +227,50 @@ fn main() -> ! {
     esp_alloc::heap_allocator!(size: 72 * 1024);
 
     let timg0 = TimerGroup::new(peripherals.TIMG0);
-    let _init = esp_wifi::init(
-        timg0.timer0,
-        esp_hal::rng::Rng::new(peripherals.RNG),
-        peripherals.RADIO_CLK,
-    )
-    .unwrap();
+    let _init = esp_wifi::init(timg0.timer0, esp_hal::rng::Rng::new(peripherals.RNG)).unwrap();
     println!("Did stuff from the template");
 
     let mut pos: isize = 0;
 
     // Menu system initialization
     let menu_items = [
-        MenuItem { text: "Item 1", value: Some(1) },
-        MenuItem { text: "Item 2", value: Some(2) },
-        MenuItem { text: "Item 3", value: Some(3) },
-        MenuItem { text: "Item 4", value: Some(4) },
-        MenuItem { text: "Item 5", value: Some(5) },
-        MenuItem { text: "Item 6", value: Some(6) },
-        MenuItem { text: "Item 7", value: Some(7) },
+        MenuItem {
+            text: "Item 1",
+            value: Some(1),
+        },
+        MenuItem {
+            text: "Item 2",
+            value: Some(2),
+        },
+        MenuItem {
+            text: "Item 3",
+            value: Some(3),
+        },
+        MenuItem {
+            text: "Item 4",
+            value: Some(4),
+        },
+        MenuItem {
+            text: "Item 5",
+            value: Some(5),
+        },
+        MenuItem {
+            text: "Item 6",
+            value: Some(6),
+        },
+        MenuItem {
+            text: "Item 7",
+            value: Some(7),
+        },
     ];
     let mut menu = Menu::new(&menu_items, 4);
 
     loop {
+        println!("Loop");
         color.hue = pos as u8;
         data = [hsv2rgb(color)];
-        led.write(brightness(gamma(data.iter().cloned()), 10)).unwrap();
+        led.write(brightness(gamma(data.iter().cloned()), 10))
+            .unwrap();
 
         // Check if the button was pressed in the interrupt handler
         let button_was_pressed = critical_section::with(|cs| {
@@ -285,13 +302,8 @@ fn interrupt_handler() {
             .unwrap()
             .is_interrupt_set()
     }) {
-        let is_button_low = critical_section::with(|cs| {
-            BUTTON
-                .borrow_ref_mut(cs)
-                .as_mut()
-                .unwrap()
-                .is_low()
-        });
+        let is_button_low =
+            critical_section::with(|cs| BUTTON.borrow_ref_mut(cs).as_mut().unwrap().is_low());
 
         if is_button_low == true {
             critical_section::with(|cs| {
